d4e6cf1a72977a7a98d5380e272ca0cf6e8adc61
Shreyan Mallik <shrnmallik@gmail.com>
2021-11-25 23:00:02 -0500

    Made it fully functional

--- Calculator.java
+++ Calculator.java
@@ -34,34 +34,35 @@ public class Calculator {
         int num1;
         int num2;
         int result = 0;
-        while (end == false) {
-            while (!gotCorrect) {
-                System.out.println("Enter your operatio (+,-,*,/)");
-                whatTheUserInputs = userInput.next();
-                System.out.println("Enter 1st number");
-                num1 = userInput.nextInt();
-                System.out.println("Enter 2nd number");
-                num2 = userInput.nextInt();
-                try {
-                    if (whatTheUserInputs.equals("+"))  {result = Addition(num1,num2); gotCorrect =true; }
-                    else if (whatTheUserInputs.equals("-")) {result = Substraction(num1, num2); gotCorrect =true; }
-                    else if (whatTheUserInputs.equals("*")) {result = Multiplication(num1, num2); gotCorrect =true; }
-                    else if (whatTheUserInputs.equals("/")) {result = Division(num1, num2); gotCorrect =true; }
-                    else {
-                        System.out.println("Enter a valid operation");
-                    }                         
+        boolean gotResult = false;
+        
+        while (!gotCorrect) {
+            try {
+                while (end == false) {
+                    System.out.println("Enter your operation (+,-,*,/)");
+                    whatTheUserInputs = userInput.next();
+                    System.out.println("Enter 1st number");
+                    num1 = userInput.nextInt();
+                    System.out.println("Enter 2nd number");
+                    num2 = userInput.nextInt();
+                    if (whatTheUserInputs.equals("+"))  {result = Addition(num1,num2); gotResult = true;}
+                    else if (whatTheUserInputs.equals("-")) {result = Substraction(num1, num2); gotResult = true;}
+                    else if (whatTheUserInputs.equals("*")) {result = Multiplication(num1, num2); gotResult = true;}
+                    else if (whatTheUserInputs.equals("/")) {result = Division(num1, num2); gotResult = true;}
+                    else {  System.out.println("Enter a valid operation"); }
+                    if (gotResult) {                               
+                        System.out.println("The result of your operation is " + result);
+                        result = 0;
+                    }    
+                    System.out.println("Press 'X' if you want to exit");
+                    if (userInput.next().toUpperCase().equals("X")) end = true;  
                 }
-                catch (Exception e) {
-                    System.out.println("Please enter a valid number");
-                }
+                gotCorrect = true;
             }
-
-            System.out.println("The result of your operation is " + result);
-            result = 0;     
-            System.out.println("Press 'X' if you want to exit");
-            if (userInput.next().toUpperCase().equals("X")) end = true;          
+            catch ( Exception e ) {
+                System.out.println("Please enter again:"); 
+            }
         }
-
     }
 
 }
